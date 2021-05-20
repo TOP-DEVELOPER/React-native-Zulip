zulip/zulip-mobile

commit 70e9ad9d47ae2e192558cb4caa9ae8781251b926

android: Skip sharing intent when it's reused after quitting.
The android recents drawer is basically a log of intents. When we
finish sending a 'share' from our app, the original intent that
launched the sharing UI still appears in this log. So, detect if the
app was launched from the recents drawer after quitting, and if so,
don't show the sharing UI.

This prevents the app from showing the sharing UI when it's resumed
after completing/cancelling a share.

android/app/src/main/java/com/zulipmobile/MainActivity.kt
@@ -43,6 +43,11 @@ open class MainActivity : ReactActivity() {
     }
 
     private fun handleSend(intent: Intent) {
+        // Intent is reused after quitting, skip it.
+        if ((getIntent().flags and Intent.FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY) != 0) {
+            return;
+        }
+
         val params: WritableMap = try {
             getParamsFromIntent(intent)
         } catch (e: ShareParamsParseException) {

TOP-DEVELOPER/React-native-Zulip

commit 6a70deff1891cd95b10fb43f9db24d97b909f60c

sharing: Fix bug when app is launched from history.
This was required because of the following bug:
1. share from any app to zulip.
2. cancel the share.
3. return back to zulip by selecting it from minimized apps.
4. now go back to the previous app used to share to zulip.
5. share something to zulip again.
6. the sharing screen will not be visible.

Intended behavior: Sharing screen should be visible in step 6

This bug was possibly introduced in:

70e9ad9d47ae2e192558cb4caa9ae8781251b926

The condition checking `FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY` in the code
that commit introduces, checks MainActivity's intent (intent the app was
originally started with). Instead it should check the intent handleSend
was called with.

This caused the intents coming via `onNewIntent` to be ignored.

android/app/src/main/java/com/zulipmobile/MainActivity.kt
@@ -64,7 +64,7 @@ open class MainActivity : ReactActivity() {
 
     private fun handleSend(intent: Intent) {
         // Intent is reused after quitting, skip it.
-        if ((getIntent().flags and Intent.FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY) != 0) {
+        if ((intent.flags and Intent.FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY) != 0) {
             return;
         }
 